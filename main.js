ed42a985b29a4f31f654f930a13a2045d6bf502c
JaredPetersen2003 <jared.petersen9@gmail.com>
2022-01-09 13:22:15 +0200

    changed branding, fix stickynav, add owl carousel

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+$(document).ready(function() {
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    smartSpeed: 500,
+    margin: 30,
+    autoplayHoverPause: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+  })
+});
+
 window.addEventListener('scroll', function(){
   var scroll_y = this.scrollY + (screen.height / 1.8);
   var alertOn = document.getElementById('counter-stats').offsetTop;
   console.log(scroll_y);
   if(scroll_y > alertOn){
-    console.log("ds");
     countUp();
     this.removeEventListener('scroll', arguments.callee, false);
   }
